4837de031460e402ea2b24c2024ebef45412f8e9
alexbasista <abasista@hashicorp.com>
2021-07-27 15:41:46 -0400

    initial

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    tfe = {
+      source  = "hashicorp/tfe"
+      version = "0.25.3"
+    }
+  }
+
+  experiments = [module_variable_optional_attrs]
+}
+
+module "tfe-workspace" {
+  source  = "tfelab.is.hashicorpdemo.com/admin/workspacer/tfe"
+  version = "0.1.0"
+
+  organization   = "admin"
+  workspace_name = "my-new-tfe-ws"
+  workspace_desc = "Description of my new TFE Workspace."
+
+  tfvars = {
+    teststring = "iamstring"
+    testlist   = ["1", "2", "3"]
+    testmap    = { "a" = "1", "b" = "2", "c" = "3" }
+  }
+}
