4c5807a7015d5ece412cfcbb091fa25a23f70fef
alexbasista <abasista@hashicorp.com>
2021-07-27 15:44:18 -0400

    add workspace

--- main.tf
+++ main.tf
@@ -23,3 +23,18 @@ module "tfe-workspace" {
     testmap    = { "a" = "1", "b" = "2", "c" = "3" }
   }
 }
+
+module "tfe-workspace2" {
+  source  = "tfelab.is.hashicorpdemo.com/admin/workspacer/tfe"
+  version = "0.1.0"
+
+  organization   = "admin"
+  workspace_name = "my-new-tfe-ws2"
+  workspace_desc = "Description of my new TFE Workspace."
+
+  tfvars = {
+    teststring = "iamstring"
+    testlist   = ["1", "2", "3"]
+    testmap    = { "a" = "1", "b" = "2", "c" = "3" }
+  }
+}
